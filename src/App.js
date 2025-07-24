e34482c5809bdd7a28933dff75e91f31799783b3
Anneke <122925139+anneke44@users.noreply.github.com>
2025-07-24 14:45:00 +0200

    Created basic HTML structure with fake data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./Footer";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Weather />
         <Footer />
       </div>
     </div>
